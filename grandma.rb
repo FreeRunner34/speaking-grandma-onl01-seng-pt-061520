db3c641c847e5d216b313789e2ed3cfc4f9f6526
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-19 06:03:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,7 @@
 
 
 def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
-       if phrase == phrase.upcase
-     return "NO, NOT SINCE 1938!"
-   end
+ 
    
       if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
